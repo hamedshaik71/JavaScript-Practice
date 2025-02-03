20a0e782e75ea4f92e1e27252a739a1012d2da89
Hamedulla Shaik <hamedshaik771@gmail.com>
2025-02-03 21:18:55 +0530

    Friend Request 2

---
+++ Friend Request Page - 2/scripts.js
@@ -0,0 +1,17 @@
+let isStatus = document.querySelector("h5");
+let btn = document.querySelector(".add");
+let check = 0;
+
+btn.addEventListener("click",function(){
+    if(check==0){
+        isStatus.innerHTML = "Friends";
+        isStatus.style.color = "green";
+        btn.innerHTML = "Remove Friend"
+        check = 1;
+    }else{
+        isStatus.innerHTML = "Stranger";
+        isStatus.style.color = "red";
+        btn.innerHTML = "Add Friend";
+        check=0;
+    }
+});
